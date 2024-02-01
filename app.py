af545ca50db2c0ae732b63356b9d0850abea81ac
Haidar200998 <73745784+Haidar200998@users.noreply.github.com>
2024-02-01 17:52:53 +0700

    app.py

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 import streamlit as st
 import pandas as pd
-import pickle
+from joblib import load
 
-# Memuat model yang disimpan
-model = pickle.load(open('trained_model.pkl', 'rb'))
+# Memuat model yang telah dilatih
+model = load('/content/drive/MyDrive/Skripsi 2/trained_model.joblib')
 
-# Membuat antarmuka pengguna di Streamlit
 st.title("Prediksi Harga Emas")
 
-# Membuat form input untuk fitur-fitur
+# Membuat form untuk input data
 with st.form("prediction_form"):
     st.write("Silakan masukkan informasi berikut:")
-    ihsg = st.number_input("Nilai IHSG", min_value=0.0, max_value=10000.0, value=5000.0)
-    usd_rate = st.number_input("Nilai Kurs USD", min_value=0.0, max_value=20000.0, value=14000.0)
-    inflation = st.number_input("Tingkat Inflasi", min_value=0.0, max_value=100.0, value=3.0)
+    ihsg = st.number_input("Nilai IHSG", value=6000.0)
+    usd_rate = st.number_input("Nilai Kurs USD", value=14000.0)
+    inflation = st.number_input("Tingkat Inflasi", value=3.0)
+    
+    # Tombol untuk melakukan prediksi
     submit_button = st.form_submit_button("Prediksi")
 
-# Proses input dan buat prediksi menggunakan model
 if submit_button:
+    # Menyiapkan data untuk prediksi
     input_data = pd.DataFrame([[ihsg, usd_rate, inflation]], columns=['IHSG', 'Kurs Jual', 'Data Inflasi'])
+    
+    # Melakukan prediksi menggunakan model
     prediction = model.predict(input_data)
-
-    # Tampilkan hasil prediksi
-    st.subheader("Hasil Prediksi Harga Emas:")
-    st.write("Prediksi Harga Emas:", prediction[0])
+    
+    # Menampilkan hasil prediksi
+    st.write(f"Prediksi harga emas: Rp{prediction[0]:,.2f}")
